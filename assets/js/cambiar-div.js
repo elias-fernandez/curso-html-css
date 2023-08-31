188ee2f8d0f9c328562f115c88c1930b7674ba6f
elias-fernandez <142787057+elias-fernandez@users.noreply.github.com>
2023-08-31 09:16:51 +0200

    Añadir ejemplo: Cambiar <div> con JS

---
+++ assets/js/cambiar-div.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function()  {
+
+  let divElemento = document.getElementById("mi-div");
+
+  let botonCambiar = document.getElementById("cambiar-div");
+
+  botonCambiar.addEventListener("click", function() {
+    divElemento.textContent= "uevo Contenido del Div";
+    divElemento.style.backgroundColor = "yellow";
+    divElemento.style.color = "red";
+  });
+});
